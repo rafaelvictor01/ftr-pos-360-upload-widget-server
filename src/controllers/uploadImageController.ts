2954e38919abaae5c0ebe61dbd58b471af1519de
rafaelvictor01 <rafaelvictor.bernardes@gmail.com>
2025-11-12 15:33:57 -0300

    [#5] - [SETUP] - Configurando a documentação do fastity com o swagger

--- src/controllers/uploadImageController.ts
+++ src/controllers/uploadImageController.ts
@@ -1,7 +1,23 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
 
 export const uploadImageController : FastifyPluginAsyncZod = async server => {
-  server.get('/upload', () => {
-    return 'Hello World'
-  })
+  server.post(
+    '/upload',
+    {
+      schema: {
+        summary: 'Upload an Image',
+        body: z.object({ name: z.string(), password: z.string() }),
+        response: {
+          200: z.object({ uploadId: z.string() }),
+          409: z
+            .object({ message: z.string() })
+            .describe('Upload already exists.'),
+        },
+      },
+    },
+    async (_request, reply) => {
+      return reply.status(200).send({ uploadId: '' })
+    }
+  )
 }
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,7 +1,11 @@
 import { fastifyCors } from "@fastify/cors";
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
@@ -9,9 +13,26 @@ import { uploadImageController } from '@/controllers/uploadImageController'
 
 const server = fastify()
 
+// Fastify configs
+
 server.setValidatorCompiler(validatorCompiler)
 server.setSerializerCompiler(serializerCompiler)
 
+server.register(fastifyCors, { origin: '*' })
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: { info: { title: 'Upload Server', version: '1.0.0' } },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+// Controllers
+server.register(uploadImageController)
+
+// Error Handler
 server.setErrorHandler((error, _request, reply) => {
   if (hasZodFastifySchemaValidationErrors(error)) {
     return reply.status(400).send({
@@ -24,10 +45,6 @@ server.setErrorHandler((error, _request, reply) => {
 
   return reply.status(500).send({ message: 'Internal server error.' })
 })
-
-server.register(fastifyCors, { origin: '*' })
-
-server.register(uploadImageController)
 
 server.listen({ port: 3333, host: '0.0.0.0'}).then(() => {
   console.log('HTTP server running!')
