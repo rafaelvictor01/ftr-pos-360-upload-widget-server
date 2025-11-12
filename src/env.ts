735cf929839fb4799128f6064cb31a1a05695b02
rafaelvictor01 <rafaelvictor.bernardes@gmail.com>
2025-11-12 20:41:18 -0300

    [#11] - [SETUP] - Configuração do Cloudflare R2 na aplicação

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,13 @@ import z from "zod";
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_URL: z.url().startsWith('postgresql://')
+  DATABASE_URL: z.url().startsWith('postgresql://'),
+
+  CLOUDFLARE_BUCKET: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  CLOUDFLARE_ACCESS_KEY: z.string(),
+  CLOUDFLARE_PUBLIC_BUCKET_URL: z.string(),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
