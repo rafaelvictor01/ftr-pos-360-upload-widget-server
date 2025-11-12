aa9de210865d7da83fba2787281ec7a10efba01b
rafaelvictor01 <rafaelvictor.bernardes@gmail.com>
2025-11-12 16:17:43 -0300

    [#7] - [SETUP] - Configurando o Drizzle ORM com o banco de dados Postgres

---
+++ src/infra/db/schemas/uploads.ts
@@ -0,0 +1,13 @@
+import { randomUUID } from "node:crypto";
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const uploads = pgTable('uploads', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  name: text('name').notNull(),
+  remoteKey: text('remote_key').notNull().unique(),
+  remoteUrl: text('remote_key').notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull()
+})
